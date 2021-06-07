45ee7196c73a908d60b1654304aa6f7e3dfdd718
lab1_q1 <rakeshchauhan0007>
2021-06-07 20:26:40 +0545

    changed a word

--- LabOne/q_1.py
+++ LabOne/q_1.py
@@ -5,7 +5,7 @@ Every number is given on separate lines.
 
 """
 
-num1= int(input('the first number is: '))
+num1= int(input('the first num is: '))
 num2= int(input('the second number is: '))
 num3= int(input('the third number is: '))
 sum = num1 + num2 + num3
